d215e871f866f1f39d6e311fa67b5bb236461756
Nasya Oristania <nasyaoristania@Nasyas-MacBook-Pro.local>
2020-06-14 11:57:08 +0700

    add animation

---
+++ build/precache-manifest.98446bca54c49c6cecf7de94959cc6ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2660d0ab404b0606ee6315f015bd4f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d256e8e7d3967037a0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d26c61f17e4d5bf17258",
+    "url": "/static/js/2.46845599.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.46845599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d256e8e7d3967037a0",
+    "url": "/static/js/main.d11ef0da.chunk.js"
+  },
+  {
+    "revision": "8f5dc277e843a8d520e5",
+    "url": "/static/js/runtime-main.ce211479.js"
+  },
+  {
+    "revision": "59fa2ad0613a2c0fbc0c8cde4f9148c2",
+    "url": "/static/media/1437380.59fa2ad0.svg"
+  },
+  {
+    "revision": "bb55d1e7e992aa99d813c4a5bc192497",
+    "url": "/static/media/42675.bb55d1e7.svg"
+  }
+]);
